3482df3761854e760ab34f58bc04efe125e0722c
AtomX8 <bartek.fryzlewicz@gmail.com>
2023-03-08 15:10:16 +0100

    wheels

--- racing.py
+++ racing.py
@@ -19,6 +19,7 @@ car_size_transform = [0,0]
 steering_percent = [0,0]
 steering_power = 0
 diameter = 0.5 # in meters
+wheel_base = 40
 
 
 
@@ -29,10 +30,29 @@ input_map = pygame.image.load(os.path.join(sourceFileDir, 'yhy.png')).convert_al
 mini_dot = pygame.image.load(os.path.join(sourceFileDir, 'mala_kropka.png')).convert_alpha()
 white = (255, 255, 255)
 
-dot = pygame.image.load(os.path.join(sourceFileDir, 'kropka.png')).convert_alpha()
 mini_dot.set_colorkey(white) 
-dot.set_colorkey(white)
 
+class wheels:
+	global wheel,wheel_transoform,wheel_size_transform,wheel_center
+	wheel_size_transform =[0,0]
+	wheel_center = [0,0]
+	wheel = pygame.Surface((5, 10), pygame.SRCALPHA)
+	wheel.fill((0,0,0))
+	def center(self,wheel_transform):
+		wheel_size_transform[0], wheel_size_transform[1] = wheel_transform.get_size()
+		wheel_center = [player_1_position_x-(wheel_size_transform[0]/2),player_1_position_y-(wheel_size_transform[1]/2)]
+
+		return wheel_center
+	def rotation(self,degree):
+		wheel_transform = pygame.transform.rotate(wheel, degree) 
+		self.center(wheel_transform)
+		return wheel_transform,wheel_center
+
+    
+fr = wheels()
+rr = wheels()
+fl = wheels()
+rl = wheels()
 car = pygame.Surface((car_size[0], car_size[1]), pygame.SRCALPHA)
 car.fill((255,0,0))
 # bg1 = pygame.transform.scale(bg,(860,480)) 
@@ -41,17 +61,38 @@ def update_fps():
 	fps = str(int(clock.get_fps()))
 	fps_text = font.render(fps, 1, pygame.Color("coral"))
 	return fps_text
+def text():
+	text = f'velocity x: {velocity[0]} velocity y: {velocity[1]} speed: {abs(speed)} position x {round(player_1_position_x)} position y {round(player_1_position_y,2)}'
+	final_text = font.render(text, 1, pygame.Color("coral"))
+	return final_text
 # screen renderer
 def screen_draw(player_1_position_x, player_1_position_y):
-	 
-	screen.fill((250, 250, 250))# background
-	screen.blit(input_map, (17, 17)) # player 1 test
-	screen.blit(mini_dot, (50-(degree_x *40), 50 -(degree_y*40))) # player 1 test
-	screen.blit(update_fps(), (10,10))
 	car_transform = pygame.transform.rotate(car, steering_degree)
 	car_size_transform[0], car_size_transform[1] = car_transform.get_size()
+	car_center = [player_1_position_x-(car_size_transform[0]/2),player_1_position_y-(car_size_transform[1]/2)]
+	screen.fill((250, 250, 250))# background
+	screen.blit(input_map, (40, 40)) # player 1 test
+	screen.blit(mini_dot, (70-(degree_x *40), 70 -(degree_y*40))) # player 1 test
+	screen.blit(update_fps(), (10,10))
+	screen.blit(text(), (70,10))
+	screen.blit(car_transform, (car_center[0],car_center[1]))
 
-	screen.blit(car_transform, (player_1_position_x-(car_size_transform[0]/2),player_1_position_y-(car_size_transform[1]/2)))
+	screen.blit(fr.rotation(steering_degree)[0],
+		((player_1_position_x)-fr.rotation(steering_degree)[1][0]-(wheel_base*degree_x)+20*degree_y,
+   		(player_1_position_y-fr.rotation(steering_degree)[1][1]-(wheel_base*degree_y)-20*degree_x
+      	)))
+	screen.blit(rr.rotation(steering_degree)[0], 
+	    ((player_1_position_x)-rr.rotation(steering_degree)[1][0]+(wheel_base*degree_x)+20*degree_y,
+      	(player_1_position_y-rr.rotation(steering_degree)[1][1]+(wheel_base*degree_y)-20*degree_x
+		)))
+	screen.blit(fl.rotation(steering_degree)[0],
+		((player_1_position_x)-fl.rotation(steering_degree)[1][0]-(wheel_base*degree_x)-20*degree_y,
+   		(player_1_position_y-fl.rotation(steering_degree)[1][1]-(wheel_base*degree_y)+20*degree_x
+      	)))
+	screen.blit(rl.rotation(steering_degree)[0], 
+	    ((player_1_position_x)-rl.rotation(steering_degree)[1][0]+(wheel_base*degree_x)-20*degree_y,
+      	(player_1_position_y-rl.rotation(steering_degree)[1][1]+(wheel_base*degree_y)+20*degree_x
+		)))
 	pygame.display.update()
 
 # main loop
@@ -68,13 +109,6 @@ while loop:
 	elif velocity[1]<0:
 		velocity[1]-= ((velocity[1]*0.05))
 
-	# print(f'vel x: {velocity[0]} vel y: {velocity[1]}')
-
-	# if vel>0:
-	# 	vel-=5
-	# elif vel<0:
-	# 	vel+=5
-
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			loop = 0    
@@ -86,13 +120,13 @@ while loop:
 	steering_percent_final2 = 1-steering_percent_final 
 	# print(steering_percent_final2)
 	steering_power =  5* steering_percent_final  
-	if pressed_keys[K_d] and steering_percent_final != 1:
+	if pressed_keys[K_d]:
 		if steering_degree >= 0:
 			steering_degree -= steering_power
 		else:
 			steering_degree = 359
 			
-	if pressed_keys[K_a] and steering_percent_final != 1:	
+	if pressed_keys[K_a]:	
 		if steering_degree < 360:
 			steering_degree += steering_power
 			
@@ -120,8 +154,8 @@ while loop:
 	velocity[0] = int(velocity[0] + speed*degree_x)
 	velocity[1] = int(velocity[1] + speed*degree_y)
 	# print (f'velocity x: {velocity[0]} velocity y: {velocity[1]} speed: {vel}')
-	rotation = velocity[0] - velocity[1] / (diameter * math.pi)
-	print(steering_power)
+	# rotation = velocity[0] - velocity[1] / (diameter * math.pi)
+
 	clock.tick(60) # fps
 	player_1_position_x += (velocity[0]/100)
 	player_1_position_y += (velocity[1]/100)
